c8d49cee0de1967a5fd15ca9db29a2bc733a1ebd
linbojin <linbojin203@gmail.com>
2013-11-19 13:24:10 +0800

    Neural Networks (Feedforward Propagation)
    Prediction using Neural Networks (Feedforward Propagation)

--- mlclass-ex3-jin/predict.m
+++ mlclass-ex3-jin/predict.m
@@ -21,11 +21,13 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+% Add ones to the X data matrix  -jin
+X = [ones(m, 1) X];
 
-
-
-
-
+a2 = sigmoid(X * Theta1');   % 第二层激活函数输出
+a2 = [ones(m, 1) a2];        % 第二层加入b
+a3 = sigmoid(a2 * Theta2');  
+[aa,p] = max(a3,[],2);               % 返回每行最大值的索引位置，也就是预测的数字
 
 
 
